6624fbca9d5e3d0a0d1f01d30ba521fe2be4b9b1
adrianlapaz3 <adrianlapaz2010@gmail.com>
2024-06-03 22:47:50 -0300

    OK

--- R/linear_model.R
+++ R/linear_model.R
@@ -5,7 +5,8 @@
 #' @param experiment Variable_with_number_experiments,  
 #' @param train_exp Vector with experiment numbers for train model, 
 #' @param test_exp Vector with experiment numbers for test model,  
-#' @param model_degree Degree of model (1 or 2),  
+#' @param model_degree Degree of model (1 or 2),
+#' @param metric The selection metric can be RRMSE, MAPE or KGE. KGE is particularly useful when the estimate needs to accurately reflect the variability at the observed extremes
 #' @param orcutt To readjust model parameters if there is dependence (1 for true, 0 for false)
 #' @param cook_distance To eliminate outliers during modeling (1 for true, 0 for false)
 #'
@@ -39,18 +40,19 @@
 #'   experiment = V10["Experiment"], 
 #'   train_exp = c(1, 2, 3, 4, 10, 11),   
 #'   test_exp = c(5, 6, 7, 8, 9),  
-#'   model_degree = 2, 
+#'   model_degree = 2,
+#'   metric = "kge",
 #'   orcutt = 1, 
-#'     cook_distance = 1
+#'   cook_distance = 1
 #' )
 #'   
-linear_model <- function(VIs, y, experiment, train_exp, test_exp, model_degree, orcutt = 0, cook_distance = 0) {
+linear_model <- function(VIs, y, experiment, train_exp, test_exp, model_degree, metric = "kge", orcutt = 0, cook_distance = 0) {
   
-  best_RRMSE_mod1 = Inf; best_model_mod1 = 0; best_train_VI_mod1 = 0; best_coeficients_mod1 = 0; best_bands_mod1 = 0; best_pred_mod1 = 0; best_test_VI_mod1 = 0
-  best_RRMSE_mod2 = Inf; best_model_mod2 = 0; best_train_VI_mod2 = 0; best_coeficients_mod2 = 0; best_bands_mod2 = 0; best_pred_mod2 = 0; best_test_VI_mod2 = 0
-  best_RRMSE_mod3 = Inf;best_model_mod3 = 0; best_train_VI_mod3 = 0; best_coeficients_mod3 = 0; best_bands_mod3 = 0; best_pred_mod3 = 0; best_test_VI_mod3 = 0
-  best_RRMSE_mod4 = Inf;best_model_mod4 = 0; best_train_VI_mod4 = 0; best_coeficients_mod4 = 0; best_bands_mod4 = 0; best_pred_mod4 = 0; best_test_VI_mod4 = 0
-  best_RRMSE_mod5 = Inf;best_model_mod5 = 0; best_train_VI_mod5 = 0; best_coeficients_mod5 = 0; best_bands_mod5 = 0; best_pred_mod5 = 0; best_test_VI_mod5 = 0
+  best_metric_mod1 = Inf; best_model_mod1 = 0; best_train_VI_mod1 = 0; best_coeficients_mod1 = 0; best_bands_mod1 = 0; best_pred_mod1 = 0; best_test_VI_mod1 = 0
+  best_metric_mod2 = Inf; best_model_mod2 = 0; best_train_VI_mod2 = 0; best_coeficients_mod2 = 0; best_bands_mod2 = 0; best_pred_mod2 = 0; best_test_VI_mod2 = 0
+  best_metric_mod3 = Inf;best_model_mod3 = 0; best_train_VI_mod3 = 0; best_coeficients_mod3 = 0; best_bands_mod3 = 0; best_pred_mod3 = 0; best_test_VI_mod3 = 0
+  best_metric_mod4 = Inf;best_model_mod4 = 0; best_train_VI_mod4 = 0; best_coeficients_mod4 = 0; best_bands_mod4 = 0; best_pred_mod4 = 0; best_test_VI_mod4 = 0
+  best_metric_mod5 = Inf;best_model_mod5 = 0; best_train_VI_mod5 = 0; best_coeficients_mod5 = 0; best_bands_mod5 = 0; best_pred_mod5 = 0; best_test_VI_mod5 = 0
   for (degree in 1:model_degree) {
     for (a in 2:length(VIs)) {
       VI_i = VIs[,a]
@@ -172,33 +174,96 @@ linear_model <- function(VIs, y, experiment, train_exp, test_exp, model_degree, 
           }
         }
         if (pvalue < 0.05 & N > 0.05 & H > 0.05 & I > 0.05 & L > 0.05){
-          train_pred <- cbind(train_VIs[,2], x0 + x1 * I(train_VIs[,3]) + x2 * I(train_VIs[,3]^2), train_VIs[,1])
-          train_pred <- na.omit(train_pred)
-          train_RRMSEs <- NULL
-          train_exp = unique(train_pred[,3])
-          for (exp in train_exp) {
-            train_exp_data = train_pred[train_pred[,3] == exp, , drop = FALSE]
+            if (metric == "rrmse" | metric == "RRMSE" | metric == "rmse" | metric == "RMSE"){
+                train_pred <- cbind(train_VIs[,2], x0 + x1 * I(train_VIs[,3]) + x2 * I(train_VIs[,3]^2), train_VIs[,1])
+                train_pred <- na.omit(train_pred)
+                train_RRMSEs <- NULL
+                train_exp = unique(train_pred[,3])
+                for (exp in train_exp) {
+                  train_exp_data = train_pred[train_pred[,3] == exp, , drop = FALSE]
+                  
+                  train_RRMSE_exp = sqrt(mean((train_exp_data[,2] - train_exp_data[,1])^2))/mean(train_exp_data[,1])*100
+                  train_RRMSEs = cbind(train_RRMSEs, train_RRMSE_exp)
+                }
+                train_metric = max(train_RRMSEs)
+                
+                test_pred <- cbind(test_VIs[,2], x0 + x1 * I(test_VIs[,3]) + x2 * I(test_VIs[,3]^2), test_VIs[,1])
+                test_pred <- na.omit(test_pred)
+                test_RRMSEs <- NULL
+                test_exp = unique(test_pred[,3])
+                for (exp in test_exp) {
+                  test_exp_data = test_pred[test_pred[,3] == exp, , drop = FALSE]
+                  test_RRMSE_exp = sqrt(mean((test_exp_data[,2] - test_exp_data[,1])^2))/mean(test_exp_data[,1])*100
+                  test_RRMSEs = cbind(test_RRMSEs, test_RRMSE_exp)
+                }
+                test_metric = max(test_RRMSEs)}
+          
+          
+          if (metric == "mape" | metric == "MAPE" | metric == "mae" | metric == "MAE"){
             
-            train_RRMSE_exp = sqrt(mean((train_exp_data[,2] - train_exp_data[,1])^2))/mean(train_exp_data[,1])*100
-            train_RRMSEs = cbind(train_RRMSEs, train_RRMSE_exp)
-          }
-          train_RRMSE = max(train_RRMSEs)
+            train_pred <- cbind(train_VIs[,2], x0 + x1 * I(train_VIs[,3]) + x2 * I(train_VIs[,3]^2), train_VIs[,1])
+            train_pred <- na.omit(train_pred)
+            train_MAPEs <- NULL
+            train_exp = unique(train_pred[,3])
+            for (exp in train_exp) {
+              train_exp_data = train_pred[train_pred[,3] == exp, , drop = FALSE]
+              train_MAPE_exp = mean(abs((train_exp_data[,1]-train_exp_data[,2])/train_exp_data[,1])) * 100
+              train_MAPEs = cbind(train_MAPEs, train_MAPE_exp)
+            }
+            train_metric = max(train_MAPEs)
+            
+            test_pred <- cbind(test_VIs[,2], x0 + x1 * I(test_VIs[,3]) + x2 * I(test_VIs[,3]^2), test_VIs[,1])
+            test_pred <- na.omit(test_pred)
+            test_MAPEs <- NULL
+            test_exp = unique(test_pred[,3])
+            for (exp in test_exp) {
+              test_exp_data = test_pred[test_pred[,3] == exp, , drop = FALSE]
+              test_MAPE_exp = mean(abs((test_exp_data[,1]-test_exp_data[,2])/test_exp_data[,1])) * 100
+              test_MAPEs = cbind(test_MAPEs, test_MAPE_exp)
+            }
+            test_metric = max(test_MAPEs)}
           
-          test_pred <- cbind(test_VIs[,2], x0 + x1 * I(test_VIs[,3]) + x2 * I(test_VIs[,3]^2), test_VIs[,1])
-          test_pred <- na.omit(test_pred)
-          test_RRMSEs <- NULL
-          test_exp = unique(test_pred[,3])
-          for (exp in test_exp) {
-            test_exp_data = test_pred[test_pred[,3] == exp, , drop = FALSE]
-            test_RRMSE_exp = sqrt(mean((test_exp_data[,2] - test_exp_data[,1])^2))/mean(test_exp_data[,1])*100
-            test_RRMSEs = cbind(test_RRMSEs, test_RRMSE_exp)
-          }
-          test_RRMSE = max(test_RRMSEs)
           
-          RRMSE = max(train_RRMSE,test_RRMSE)
+          if (metric == "kge" | metric == "KGE"){
+            
+            train_pred <- cbind(train_VIs[,2], x0 + x1 * I(train_VIs[,3]) + x2 * I(train_VIs[,3]^2), train_VIs[,1])
+            train_pred <- na.omit(train_pred)
+            train_KGEs <- NULL
+            train_exp = unique(train_pred[,3])
+            for (exp in train_exp) {
+              train_exp_data = train_pred[train_pred[,3] == exp, , drop = FALSE]
+              train_KGE_exp = 1 - sqrt(
+                (cor(train_exp_data[,1], train_exp_data[,2]) - 1)^2 + 
+                  ((sqrt(mean((train_exp_data[,2] - mean(train_exp_data[,2]))^2))/mean(train_exp_data[,2])) / (sqrt(mean((train_exp_data[,1] - mean(train_exp_data[,1]))^2))/mean(train_exp_data[,1])) - 1)^2 +
+                  (mean(train_exp_data[,2]) / mean(train_exp_data[,1]) - 1)^2 )
+              if (train_KGE_exp > 0){train_KGE_exp = 1 - train_KGE_exp}
+              
+              train_KGEs = cbind(train_KGEs, train_KGE_exp)
+            }
+            train_metric = max(train_KGEs)
+            
+            test_pred <- cbind(test_VIs[,2], x0 + x1 * I(test_VIs[,3]) + x2 * I(test_VIs[,3]^2), test_VIs[,1])
+            test_pred <- na.omit(test_pred)
+            test_KGEs <- NULL
+            test_exp = unique(test_pred[,3])
+            for (exp in test_exp) {
+              test_exp_data = test_pred[test_pred[,3] == exp, , drop = FALSE]
+              test_KGE_exp = 1 - sqrt(
+                (cor(test_exp_data[,1], test_exp_data[,2]) - 1)^2 + 
+                  ((sqrt(mean((test_exp_data[,2] - mean(test_exp_data[,2]))^2))/mean(test_exp_data[,2])) / (sqrt(mean((test_exp_data[,1] - mean(test_exp_data[,1]))^2))/mean(test_exp_data[,1])) - 1)^2 +
+                  (mean(test_exp_data[,2]) / mean(test_exp_data[,1]) - 1)^2 )
+              test_KGEs = cbind(test_KGEs, test_KGE_exp)
+            }
+            test_metric = max(test_KGEs)}
+          
+          
+          
+          
+          best_metric = max(train_metric,test_metric)
+          
           
           # selecting the five best models
-          if (RRMSE < best_RRMSE_mod1) {best_RRMSE_mod5 = best_RRMSE_mod4; best_RRMSE_mod4 = best_RRMSE_mod3;best_RRMSE_mod3 = best_RRMSE_mod2;best_RRMSE_mod2 = best_RRMSE_mod1; best_RRMSE_mod1 = RRMSE
+          if (best_metric < best_metric_mod1) {best_metric_mod5 = best_metric_mod4; best_metric_mod4 = best_metric_mod3;best_metric_mod3 = best_metric_mod2;best_metric_mod2 = best_metric_mod1; best_metric_mod1 = best_metric
           best_model_mod5 = best_model_mod4; best_train_VI_mod5 = best_train_VI_mod4; best_bands_mod5 = best_bands_mod4;best_test_VI_mod5 = best_test_VI_mod4;
           best_model_mod4 = best_model_mod3; best_train_VI_mod4 = best_train_VI_mod3; best_bands_mod4 = best_bands_mod3;best_test_VI_mod4 = best_test_VI_mod3;
           best_model_mod3 = best_model_mod2; best_train_VI_mod3 = best_train_VI_mod2; best_bands_mod3 = best_bands_mod2;best_test_VI_mod3 = best_test_VI_mod2;
@@ -206,25 +271,25 @@ linear_model <- function(VIs, y, experiment, train_exp, test_exp, model_degree, 
           best_model_mod1 = model; best_train_VI_mod1 = train_VIs; best_test_VI_mod1 = cbind(test_VIs, pred = test_pred[,2]);
           best_bands_mod1 = colnames(VIs[a])}
           
-          if (RRMSE < best_RRMSE_mod2 & RRMSE > best_RRMSE_mod1) {best_RRMSE_mod5 = best_RRMSE_mod4; best_RRMSE_mod4 = best_RRMSE_mod3;best_RRMSE_mod3 = best_RRMSE_mod2;best_RRMSE_mod2 = RRMSE
+          if (best_metric < best_metric_mod2 & best_metric > best_metric_mod1) {best_metric_mod5 = best_metric_mod4; best_metric_mod4 = best_metric_mod3;best_metric_mod3 = best_metric_mod2;best_metric_mod2 = best_metric
           best_model_mod5 = best_model_mod4; best_train_VI_mod5 = best_train_VI_mod4; best_bands_mod5 = best_bands_mod4;best_test_VI_mod5 = best_test_VI_mod4;
           best_model_mod4 = best_model_mod3; best_train_VI_mod4 = best_train_VI_mod3; best_bands_mod4 = best_bands_mod3;best_test_VI_mod4 = best_test_VI_mod3;
           best_model_mod3 = best_model_mod2; best_train_VI_mod3 = best_train_VI_mod2; best_bands_mod3 = best_bands_mod2;best_test_VI_mod3 = best_test_VI_mod2;
           best_model_mod2 = model; best_train_VI_mod2 = train_VIs; best_test_VI_mod2 = cbind(test_VIs, pred = test_pred[,2]);
           best_bands_mod2 = colnames(VIs[a])}
           
-          if (RRMSE < best_RRMSE_mod3 & RRMSE > best_RRMSE_mod1 & RRMSE > best_RRMSE_mod2) {best_RRMSE_mod5 = best_RRMSE_mod4; best_RRMSE_mod4 = best_RRMSE_mod3;best_RRMSE_mod3 = RRMSE
+          if (best_metric < best_metric_mod3 & best_metric > best_metric_mod1 & best_metric > best_metric_mod2) {best_metric_mod5 = best_metric_mod4; best_metric_mod4 = best_metric_mod3;best_metric_mod3 = best_metric
           best_model_mod5 = best_model_mod4; best_train_VI_mod5 = best_train_VI_mod4; best_bands_mod5 = best_bands_mod4;best_test_VI_mod5 = best_test_VI_mod4;
           best_model_mod4 = best_model_mod3; best_train_VI_mod4 = best_train_VI_mod3; best_bands_mod4 = best_bands_mod3;best_test_VI_mod4 = best_test_VI_mod3;
           best_model_mod3 = model; best_train_VI_mod3 = train_VIs; best_test_VI_mod3 = cbind(test_VIs, pred = test_pred[,2]);
           best_bands_mod3 = colnames(VIs[a])} 
           
-          if (RRMSE < best_RRMSE_mod4 & RRMSE > best_RRMSE_mod1 & RRMSE > best_RRMSE_mod2 & RRMSE > best_RRMSE_mod3) {best_RRMSE_mod5 = best_RRMSE_mod4; best_RRMSE_mod4 = RRMSE
+          if (best_metric < best_metric_mod4 & best_metric > best_metric_mod1 & best_metric > best_metric_mod2 & best_metric > best_metric_mod3) {best_metric_mod5 = best_metric_mod4; best_metric_mod4 = best_metric
           best_model_mod5 = best_model_mod4; best_train_VI_mod5 = best_train_VI_mod4; best_bands_mod5 = best_bands_mod4;best_test_VI_mod5 = best_test_VI_mod4;
           best_model_mod4 = model; best_train_VI_mod4 = train_VIs; best_test_VI_mod4 = cbind(test_VIs, pred = test_pred[,2]);
           best_bands_mod4 = colnames(VIs[a])}
           
-          if (RRMSE < best_RRMSE_mod5 & RRMSE > best_RRMSE_mod1 & RRMSE > best_RRMSE_mod2 & RRMSE > best_RRMSE_mod3 & RRMSE > best_RRMSE_mod3 & RRMSE > best_RRMSE_mod4) {best_RRMSE_mod5 = RRMSE
+          if (best_metric < best_metric_mod5 & best_metric > best_metric_mod1 & best_metric > best_metric_mod2 & best_metric > best_metric_mod3 & best_metric > best_metric_mod3 & best_metric > best_metric_mod4) {best_metric_mod5 = best_metric
           best_model_mod5 = model; best_train_VI_mod5 = train_VIs; best_test_VI_mod5 = cbind(test_VIs,pred = test_pred[,2]);
           best_bands_mod5 = colnames(VIs[a])}
         } 
@@ -236,31 +301,31 @@ linear_model <- function(VIs, y, experiment, train_exp, test_exp, model_degree, 
   
   return(list(
     best_bands_mod1 = best_bands_mod1,
-    best_RRMSE_mod1 = best_RRMSE_mod1,
+    best_metric_mod1 = best_metric_mod1,
     best_model_mod1 = summary(best_model_mod1),
     best_train_VI_mod1 = best_train_VI_mod1, 
     best_test_VI_mod1 = best_test_VI_mod1,
     
     best_bands_mod2 = best_bands_mod2,
-    best_RRMSE_mod2 = best_RRMSE_mod2,
+    best_metric_mod2 = best_metric_mod2,
     best_model_mod2 = summary(best_model_mod2),
     best_train_VI_mod2 = best_train_VI_mod2, 
     best_test_VI_mod2 = best_test_VI_mod2,
     
     best_bands_mod3 = best_bands_mod3,
-    best_RRMSE_mod3 = best_RRMSE_mod3,
+    best_metric_mod3 = best_metric_mod3,
     best_model_mod3 = summary(best_model_mod3),
     best_train_VI_mod3 = best_train_VI_mod3, 
     best_test_VI_mod3 = best_test_VI_mod3,
     
     best_bands_mod4 = best_bands_mod4,
-    best_RRMSE_mod4 = best_RRMSE_mod4,
+    best_metric_mod4 = best_metric_mod4,
     best_model_mod4 = summary(best_model_mod4),
     best_train_VI_mod4 = best_train_VI_mod4, 
     best_test_VI_mod4 = best_test_VI_mod4,
     
     best_bands_mod5 = best_bands_mod5,
-    best_RRMSE_mod5 = best_RRMSE_mod5,     
+    best_metric_mod5 = best_metric_mod5,     
     best_model_mod5 = summary(best_model_mod5),
     best_train_VI_mod5 = best_train_VI_mod5, 
     best_test_VI_mod5 = best_test_VI_mod5
